7273a929ee954d51312cd490ea4886041d211e50
Shipon Hossen Raju <msshipon234@gmail.com>
2025-01-27 13:58:52 +0600

    shadcn added

--- cycle-store-frontend/tailwind.config.js
+++ cycle-store-frontend/tailwind.config.js
@@ -1,29 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#101010",
-        brand: "#C52A09",
-        gray: "#3D3D3D",
-        secondary: "#F6F6F6",
-        danger: "#DC2626",
-        bgColor: "#1C1B1B",
-
-        btnBg: "#FBF9F4",
-        btnText: "#022205",
-
-        TextGray: "#E7E7E7",
-        txtPrimaryColor: "#4E5152",
-      },
-      fontFamily: {
-        poppinsSans: ["Poppins", "sans-serif"],
-      },
-      boxShadow: {
-        shadowPrimary: "4px 4px 32.3px -13px rgba(0, 0, 0, 0.25)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			brand: '#C52A09',
+  			gray: '#3D3D3D',
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			danger: '#DC2626',
+  			bgColor: '#1C1B1B',
+  			btnBg: '#FBF9F4',
+  			btnText: '#022205',
+  			TextGray: '#E7E7E7',
+  			txtPrimaryColor: '#4E5152',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			poppinsSans: [
+  				'Poppins',
+  				'sans-serif'
+  			]
+  		},
+  		boxShadow: {
+  			shadowPrimary: '4px 4px 32.3px -13px rgba(0, 0, 0, 0.25)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
